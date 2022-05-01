119b4f25d3ba701c377384963bd2c86c7d49b217
Akio <481158831@qq.com>
2022-05-01 13:35:08 +0800

    pdfbox init

---
+++ src/main/java/com/shenhua/pdfboxctl/PdfboxctlApplication.java
@@ -0,0 +1,13 @@
+package com.shenhua.pdfboxctl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfboxctlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfboxctlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shenhua/pdfboxctl/PdfboxctlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shenhua.pdfboxctl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfboxctlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
